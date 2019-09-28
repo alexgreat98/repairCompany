9511cb4cd1a09e4baa21708584a02972db76da16
nicotine <n@gmail.com>
2019-09-28 15:17:22 +0300

    немного контроллеров и моделей

--- resources/js/components/AppAdmin.vue
+++ resources/js/components/AppAdmin.vue
@@ -261,22 +261,22 @@
             dialog: false,
             drawer: null,
             items: [
-                { icon: 'contacts', text: 'Contacts' },
+                { icon: 'contacts', text: 'Пользователи' },
                 { icon: 'history', text: 'Frequently contacted' },
-                { icon: 'content_copy', text: 'Duplicates' },
+                { icon: 'content_copy', text: 'Галерея' },
+                 // {
+                 //     icon: 'keyboard_arrow_up',
+                 //     'icon-alt': 'keyboard_arrow_down',
+                 //     text: 'Labels',
+                 //     model: true,
+                 //     children: [
+                 //         { icon: 'add', text: 'Create label' },
+                 //     ],
+                 // },
                 {
                     icon: 'keyboard_arrow_up',
                     'icon-alt': 'keyboard_arrow_down',
-                    text: 'Labels',
-                    model: true,
-                    children: [
-                        { icon: 'add', text: 'Create label' },
-                    ],
-                },
-                {
-                    icon: 'keyboard_arrow_up',
-                    'icon-alt': 'keyboard_arrow_down',
-                    text: 'More',
+                    text: 'Разделы',
                     model: false,
                     children: [
                         { text: 'Import' },
@@ -286,11 +286,11 @@
                         { text: 'Other contacts' },
                     ],
                 },
-                { icon: 'settings', text: 'Settings' },
-                { icon: 'chat_bubble', text: 'Send feedback' },
-                { icon: 'help', text: 'Help' },
-                { icon: 'phonelink', text: 'App downloads' },
-                { icon: 'keyboard', text: 'Go to the old version' },
+                { icon: 'settings', text: 'Параметры' },
+                 // { icon: 'chat_bubble', text: 'Send feedback' },
+                 // { icon: 'help', text: 'Help' },
+                 // { icon: 'phonelink', text: 'App downloads' },
+                 // { icon: 'keyboard', text: 'Go to the old version' },
             ],
         }),
     }
